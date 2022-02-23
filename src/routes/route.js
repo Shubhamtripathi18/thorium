a86f537aa7bd34f7be9bafe16e1173fbe087cc6f
Shubhamtripathi18 <shubhamtripathi800@gmail.com>
2022-02-23 23:27:02 +0530

    my second assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,69 +1,84 @@
-let obj= require('./logger1/logger')
-let help= require('./util/helper')
 const express = require('express');
-const { chunk } = require('lodash');
-const { tail } = require('lodash');
-const lodash = require('lodash')
-const {union} = require('lodash');
-const {fromPairs} = require('lodash')
-
-
 const router = express.Router();
 
-router.get('/hi-there', function (req, res) {
-    help.currentdate('thorium')
-    console.log(help)
-   res.send('welcome to functionup. im sunny chakrawarty and a part of functionup thorium')
+router.get('/test-me', function (req, res) {
+    res.send('My first ever api!')
 });
 
-
-
-router.get('/Hello', function (req, res) {
-    let month= ["january", "February", "march", "april", "May", "June", "July", "august", "sept", "oct",  "nov", "dec"]
-
-       console.log(lodash.chunk(month,4)) 
-    res.send('welcome to functionup. im sunny chakrawarty and a part of functionup thorium')
+router.get('', function(req,res){
+    res.send('Server Working Properly')
 });
-
-router.get('/Hello3', function (req, res) {
-    let movie= [['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']]
-
-       console.log(lodash.fromPairs(movie)) 
-    res.send('welcome to functionup. im sunny chakrawarty and a part of functionup thorium')
-});
-
-
-router.get('/Hello2', function (req, res) {
     
-    let arr1= [1,2,3,4,5]
-    let arr2= [5,6,7,8,9]
-    let arr3= [9,10,11,12,13]
-    let arr4= [13,14,15,16,17]
-    let arr5= [17,18,19,20,21]
-   
-    console.log(lodash.union(arr1,arr2,arr3,arr4,arr5)) 
-    res.send('welcome to functionup. im sunny chakrawarty and a part of functionup thorium')
+router.get('/movies', function(req,res) {
+    const movies = ['Spiderman No Way Home', 'KGF Chapter 2', 'Pushpa', 'RRR'] ;
+        res.send(movies) ;    
 });
 
-router.get('/Hello1', function (req, res) {
-    let oddNumber= [1,3,5,7,9,11,13,15,17,19]
-    
+router.get('/movies/:number', function(req,res) {
+    const movies  = ['Spiderman No Way Home', 'KGF Chapter 2', 'Pushpa', 'RRR'] ;
+    const movie = req.params.number
+    if(movie >= movies.length){
+        res.send('Invalid Index')
+    } else {
+    res.send(movies[movie])
+    }
+});
 
-       console.log(lodash.tail(oddNumber)) 
-    res.send('welcome to functionup. im sunny chakrawarty and a part of functionup thorium')
+router.get('/films', function(req,res){
+    const films = [ {
+        "id" : 1,
+        "name" : "Shaadi Mein Zaroor Aana"
+       }, {
+        "id": 2,
+        "name" : "Soun Ki Titu Ki Sweety"
+       }, {
+        "id": 3,
+        "name" : "Hum Aapke Hain Koun"
+       }, {
+        "id": 4,
+        "name": "M.S. Dhoni: The Untold Story"
+       }]
+       
+       res.send(films)
+
+});
+
+
+router.get('/films/:filmId', function(req,res){
+    const films = [ {
+        "id" : 1,
+        "name" : "Shaadi Mein Zaroor Aana"
+       }, {
+        "id": 2,
+        "name" : "Soun Ki Titu Ki Sweety"
+       }, {
+        "id": 3,
+        "name" : "Hum Aapke Hain Koun"
+       }, {
+        "id": 4,
+        "name": "M.S. Dhoni: The Untold Story"
+       }]
+
+       const filmId = req.params.filmId
+    //    let found = false;
+    //    for(let i=0; i<films.length;i++){
+    //        if(films[i].id == filmId){
+    //            found = true
+    //            res.send(films[i])
+    //        }
+    //    }
+
+        console.log(filmId)
+
+       for(let i=0; i<films.length; i++) {
+          if(films[i].id == filmId){
+           res.send(films[i].name)
+           return 
+          } 
+        }
+        
+        res.send('No movie exists with this id')
+        
 });
 
 module.exports = router;
-// adding this comment for no reason
-
-//router.get('/test-me', function (req, res) {
-    //obj.log('thorium')
-    //console.log(obj.url)
-   // res.send('welcome to functionup. im sunny chakrawarty and a part of functionup thorium')
-//});
-
-
-
-
-
-
