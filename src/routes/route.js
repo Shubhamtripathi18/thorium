9017d1dbd9736e32090ff7aefbe6ff1aada2225d
Ankitsrivastav412 <ankitsrivastava41097@gmail.com>
2022-02-23 23:25:19 +0530

    message1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,84 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+//router.get('/test-me', function (req, res) {
+  //  res.send('My first ever api!')
+//});
+
+//router.get('', function(req,res){
+  //  res.send('Server Working Properly')
+//});
+    
+router.get('/movies', function(req,res) {
+    const movies = ['the game', 'shawshank', 'special26', 'Wanted'] ;
+        res.send(movies) ;    
+});
+
+router.get('/movies/:number', function(req,res) {
+    const movies  = ['the game', 'shawshank', 'special26', 'Wanted'] ;
+    const movie = req.params.number
+    if(movie >= movies.length){
+        res.send('Invalid Index')
+    } else {
+    res.send(movies[movie])
+    }
+});
+
+router.get('/films', function(req,res){
+    const films = [ {
+        "id" : 1,
+        "name" : "Shaadi Mein Zaroor Aana"
+       }, {
+        "id": 2,
+        "name" : "Soun Ki Titu Ki Sweety"
+       }, {
+        "id": 3,
+        "name" : "Hum Aapke Hain Koun"
+       }, {
+        "id": 4,
+        "name": "M.S. Dhoni: The Untold Story"
+       }]
+       
+       res.send(films)
+
+});
+
+
+router.get('/films/:filmId', function(req,res){
+    const films = [ {
+        "id" : 1,
+        "name" : "Shaadi Mein Zaroor Aana"
+       }, {
+        "id": 2,
+        "name" : "Soun Ki Titu Ki Sweety"
+       }, {
+        "id": 3,
+        "name" : "Hum Aapke Hain Koun"
+       }, {
+        "id": 4,
+        "name": "M.S. Dhoni: The Untold Story"
+       }]
+
+       const filmId = req.params.filmId
+    //    let found = false;
+    //    for(let i=0; i<films.length;i++){
+    //        if(films[i].id == filmId){
+    //            found = true
+    //            res.send(films[i])
+    //        }
+    //    }
+
+        console.log(filmId)
+
+       for(let i=0; i<films.length; i++) {
+          if(films[i].id == filmId){
+           res.send(films[i].name)
+           return 
+          } 
+        }
+        
+        res.send('No movie exists with this id')
+        
 });
 
 module.exports = router;
